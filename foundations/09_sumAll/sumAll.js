7e1cb6abb5dd3808ad4199b3563f8e30385523c5
KamilPiech <718133+KamilPiech@users.noreply.github.com>
2025-12-30 10:49:34 +0100

    Finish 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) return 'ERROR'
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR'
 
+    if (a > b) {
+        let tmp = a
+        a = b
+        b = tmp
+    }
+
+    let totalValue = 0
+    for (let x = a; x <= b; x++) {
+        totalValue +=x
+    }
+    
+    return totalValue
 };
 
 // Do not edit below this line
